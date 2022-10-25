dc74d55b0775e024c420352a164a7b40b9067a13
allfiction100806210913 <lili10080621@gmail.com>
2022-10-25 14:18:55 +0900

    update:code

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,6 +1,5 @@
 class Library < ApplicationRecord
   has_one_attached :image
-  belongs_to :user
   validates :card_type, presence: true
   validates :numbers, presence: true
   validates :image, presence: true
