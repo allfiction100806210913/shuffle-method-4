62d2230c576719e891877039197c2b85af39c203
allfiction100806210913 <lili10080621@gmail.com>
2022-10-25 13:42:11 +0900

    update: active-strage

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,5 +1,7 @@
 class Library < ApplicationRecord
+  has_one_attached :image
   belongs_to :user
   validates :card_type, presence: true
   validates :numbers, presence: true
+  validates :image, presence: true
 end
